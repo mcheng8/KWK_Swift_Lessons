e29e101c70e514fdf100b4ba726a6bcc6dc84b49
mcheng8 <50517517+mcheng8@users.noreply.github.com>
2020-02-12 13:34:41 -0500

    Add files via upload

---
+++ 02_Conditionals.playground/Contents.swift
@@ -0,0 +1,121 @@
+import UIKit
+
+/*
+1.2 - Coding with Conditionals
+
+Learning Goals:
+1.  Using comparison operators to compare variables, strings, and numbers
+2.Write and understand conditional logic
+*/
+
+//
+//
+//Code-a-long: Comparision Operator
+
+//1. Write at least 2 expressions using the "less than" operator
+
+
+//2. Write at least 2 expressions using the "greater than" operator
+
+
+//3. Write at least 2 expressions using the "less than or equal to" operator
+
+
+//4. Write at least 2 expressions using the "greater than or equal to" operator.
+
+
+//5. Write at least 2 expressions using the "equivalent" operator.
+
+
+//6. Write at least 2 expressions using the  "not equal" operator.
+
+
+//
+//
+//Code-a-long:  Using Comparison Operators to Evaluate Variables.
+
+//
+//Declaring the variable "language" and assinging a value
+
+
+//1. Write 2 conditions involving the variable "language" that evaluates to false
+
+
+//2. Write 2 conditions involving the variable "language" that evaluates to true
+
+
+//
+//Declaring the variables "year" and "nextYear" and assigning a value
+
+
+//3. Write 2 conditions involving the variable "year" and the "greater than" operator that evaluates to true
+
+
+//4. Write 2 conditions involving the variable "year" and the "not equal" operator that evaluates to true
+
+
+//5. Write a condition involving the variable "year", the variable "nextYear" the "less than" operator that evaluates to true
+
+
+//
+//
+//Try It:  Using Comparision Oprerators
+
+//
+//Declaring the variables "firstName" and "age" and assigning them a value.
+
+
+//Check the appropriate variables for the following things:
+//a. Is your name the same as "Karlie"?
+
+
+//b. Is your age the same as 15?
+
+
+//c. Is your age not the same as 15?
+
+
+//d. Is your age greater than 7?
+
+
+//e. Is your age less than or equal to 10?
+
+
+//g. What is the remainder when you age is divided by 2?  (Helpful Hint:  Look up an operator called modulo)
+
+
+//
+//Challenge:  Have you ever tried to create a password, but the application told you you need at least 8 characters? Time to solve a real-world challenge!
+// Declare the variable "fakePassword" and assign it a string of your choice.
+
+
+//Use Google to look up a way to check the number of characters for the value stored in fakePassword
+
+
+
+//
+//
+//Code-a-long:  If Statements
+//Directions: Describe the function or purpose of each line of the if statement
+
+var dogAge = 15;
+
+if dogAge < 2 {
+  print("You are a puppy")
+}
+else if dogAge > 12 {
+  print("You are awesome")
+}
+else {
+  print("You are old")
+}
+
+//Try It: Conditionals
+
+
+
+//Medium Challenge
+
+
+//Spicy Challenge
+
